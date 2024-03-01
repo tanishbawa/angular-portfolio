a537cf468b529e1a98b9fab1a6c2d7ec04c94686
Tanish Bawa <pratik@Tanish-Bawa-B514s-MacBook-Pro.local>
2024-03-01 18:59:25 +0530

    completed basic layout

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-about',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  showEducation: boolean = true;
+  showProjects: boolean = true;
+
+  projects = [
+    { name: 'Project 1', description: 'Static discription for now' },
+    { name: 'Project 2', description: 'Static discription for now' },
+    { name: 'Project 3', description: 'Static discription for now' },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-contacts',
+  imports: [
+    NavbarComponent,
+    FooterComponent,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+  styleUrls: ['./contacts.component.scss'],
 })
 export class ContactsComponent implements OnInit {
-
-  constructor() { }
+  userForm: any;
+  constructor(public formbuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.userForm = this.formbuilder.group({
+      name: [null],
+      email: [null],
+      message: [null],
+    });
   }
 
+  onSubmit() {}
 }
